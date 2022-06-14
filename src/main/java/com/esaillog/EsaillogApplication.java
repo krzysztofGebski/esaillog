2fbac4129d860b87d8166b5669cf1ca1e35dfd7e
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2022-06-14 21:49:58 +0200

    first commit

---
+++ src/main/java/com/esaillog/EsaillogApplication.java
@@ -0,0 +1,13 @@
+package com.esaillog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsaillogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsaillogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esaillog/EsaillogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esaillog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsaillogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
