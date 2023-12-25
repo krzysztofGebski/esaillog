34f484c698548fc62d3d34a04621bfd4adb0731e
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2023-12-25 16:33:55 +0100

    Update Spring Boot version and add CrossOrigin annotation

--- src/main/java/com/esaillog/sailor/SailorController.java
+++ src/main/java/com/esaillog/sailor/SailorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/sailors")
 @RequiredArgsConstructor
